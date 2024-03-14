482d8e97d64122a812ec3edfc44eda1e5f932332
Bella Kurnia Amellia <150129572+bella2105@users.noreply.github.com>
2024-03-14 15:18:11 +0700

    Update ex10_var.dart
    Perubahan ex10_var.dart

--- ex10_var.dart
+++ ex10_var.dart
@@ -1,16 +1,29 @@
-// variabel var
+//variabel var
 void main(List<String> args) {
+  // Mendeklarasikan variabel 'x' tanpa menentukan tipe data secara eksplisit (menggunakan 'var').
   var x;
+  // Memberikan nilai 1 kepada variabel 'x'. Dart akan menentukan tipe data 'x' menjadi int berdasarkan nilai yang diberikan.
   x = 1;
-  print("Nilai x\t: " + x.toString());
-  print("Tipe x\t: " + x.runtimeType.toString());
+  // Mencetak nilai dan tipe data variabel 'x' setelah diberi nilai int.
+  print("Nilai a\t: " + x.toString());
+  print("Tipe a\t: " + x.runtimeType.toString());
+  // Memberikan nilai 123.456 kepada variabel 'x'. Dart akan menentukan tipe data 'x' menjadi double berdasarkan nilai yang diberikan.
   x = 123.456;
-  print("Nilai x\t: " + x.toString());
-  print("Tipe x\t: " + x.runtimeType.toString());
+  // Mencetak nilai dan tipe data variabel 'x' setelah diberi nilai double.
+  print("Nilai a\t: " + x.toString());
+  print("Tipe a\t: " + x.runtimeType.toString());
+
+  // Memberikan nilai true kepada variabel 'x'. Dart akan menentukan tipe data 'x' menjadi bool berdasarkan nilai yang diberikan.
   x = true;
-  print("Nilai x\t: " + x.toString());
-  print("Tipe x\t: " + x.runtimeType.toString());
+
+  // Mencetak nilai dan tipe data variabel 'x' setelah diberi nilai boolean.
+  print("Nilai a\t: " + x.toString());
+  print("Tipe a\t: " + x.runtimeType.toString());
+
+  // Memberikan nilai 'Dart' kepada variabel 'x'. Dart akan menentukan tipe data 'x' menjadi String berdasarkan nilai yang diberikan.
   x = 'Dart';
-  print("Nilai x\t: " + x.toString());
-  print("Tipe x\t: " + x.runtimeType.toString());
+
+  // Mencetak nilai dan tipe data variabel 'x' setelah diberi nilai String.
+  print("Nilai a\t: " + x.toString());
+  print("Tipe a\t: " + x.runtimeType.toString());
 }
